a79db25d4a2b85c248ad78572a4d186f5c4e08b5
Vishwakse <vishwaksen2275@gmail.com>
2026-01-12 13:04:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    panel.classList.add("active");
+
+    const imgText = panel.children[1];
+    imgText.style.display = "block";
+
+    panels.forEach((otherPanel) => {
+      const otherImgText = otherPanel.children[1];
+      otherImgText.style.display = "block";
+      if (otherPanel !== panel) {
+        otherPanel.classList.remove("active");
+        otherImgText.style.display = "none";
+      }
+    });
+  });
+});
